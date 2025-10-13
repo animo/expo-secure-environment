1e10cf003ee037c206ccad854bbe2f2b60431410
Timo Glastra <timo@animo.id>
2025-10-13 12:30:46 +0200

    feat: add native error message
    Signed-off-by: Timo Glastra <timo@animo.id>

--- src/SecureEnvironmentError.ts
+++ src/SecureEnvironmentError.ts
@@ -1,48 +1,68 @@
 export class SecureEnvironmentError extends Error {
-  public constructor(message: string, keyId: string, nativeError: string) {
-    super(message, { cause: { keyId } })
+  public constructor(
+    message: string,
+    public keyId: string,
+    public nativeError: string
+  ) {
+    super(message)
+  }
+
+  public toJSON() {
+    return {
+      message: this.message,
+      keyId: this.keyId,
+      nativeError: this.nativeError,
+    }
   }
 
   static fromNative(keyId: string, error: string) {
     if (error.includes('KeychainQueryError')) {
-      return new KeychainQueryError('Error while querying for key', keyId, error)
+      return new KeychainQueryError(`Error while querying for key ${keyId}: ${error}`, keyId, error)
     }
     if (error.includes('KeyNotFound')) {
-      return new KeyNotFoundError('Key not found', keyId, error)
+      return new KeyNotFoundError(`Key ${keyId} not found: ${error}`, keyId, error)
     }
     if (error.includes('KeyAlreadyExists')) {
-      return new KeyAlreadyExistsError('Key already exists', keyId, error)
+      return new KeyAlreadyExistsError(`Key ${keyId} already exists: ${error}`, keyId, error)
     }
     if (error.includes('CouldNotConvertIntoPublicKey')) {
-      return new CouldNotConvertIntoPublicKey('Could not convert key into public key', keyId, error)
+      return new CouldNotConvertIntoPublicKey(`Could not convert key ${keyId} into public key: ${error}`, keyId, error)
     }
     if (error.includes('CouldNotConvertSignatureToRaw')) {
       return new CouldNotConvertIntoExternalRepresentation(
-        'Could not convert the signature into its raw format',
+        `Could not convert the signature into its raw format: ${error}`,
         keyId,
         error
       )
     }
     if (error.includes('CouldNotCreateSignature')) {
-      return new CouldNotCreateSignature('An error occurred ', keyId, error)
+      return new CouldNotCreateSignature(
+        `An error occurred while creating a signature with key ${keyId}: ${error}`,
+        keyId,
+        error
+      )
     }
     if (error.includes('BiometricAuthenticationFailed')) {
-      return new BiometricAuthenticationFailed('Biometric Authentication Failed', keyId, error)
+      return new BiometricAuthenticationFailed(
+        `Biometric Authentication Failed for key ${keyId}: ${error}`,
+        keyId,
+        error
+      )
     }
     if (error.includes('NoHardwareKeyStore')) {
-      return new NoHardwareKeyStore('No support for the hardware key store', keyId, error)
+      return new NoHardwareKeyStore(`No support for the hardware key store: ${error}`, keyId, error)
     }
     if (error.includes('NoSignatureOnCryptoObject')) {
-      return new NoSignatureOnCryptoObject('No signature on the crypto object', keyId, error)
+      return new NoSignatureOnCryptoObject(`No signature on the crypto object: ${error}`, keyId, error)
     }
     if (error.includes('NotExecutedFromMainThread')) {
-      return new NotExecutedFromMainThread('Function must be executed from the main thread', keyId, error)
+      return new NotExecutedFromMainThread(`Function must be executed from the main thread: ${error}`, keyId, error)
     }
     if (error.includes('CouldNotGenerateKeyPair')) {
-      return new CouldNotGenerateKeyPair('Could not generate the key pair', keyId, error)
+      return new CouldNotGenerateKeyPair(`Could not generate the key pair: ${error}`, keyId, error)
     }
 
-    return new SecureEnvironmentError('Unable to determine error', keyId, error)
+    return new SecureEnvironmentError(`Unable to determine error: ${error}`, keyId, error)
   }
 }
 
